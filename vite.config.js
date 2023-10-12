8194987579aab54b64716cfbf80bbf0f7966d212
Valery Shcherbinin <fundroin@mail.ru>
2023-10-12 12:46:59 +0500

    инициализация проекта

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url';
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+})
