a9bd1c3e219053a298220fb747cdb2f9e3dca28a
Valery Shcherbinin <fundroin@mail.ru>
2023-10-12 14:12:08 +0500

    1 добавляет компонент базовой кнопки

---
+++ src/components/ui/base-button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+      :class="['base-button', `base-button--${color}`]"
+      @click="clickHandler"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'base-button',
+  props: {
+    label: {
+      type: String,
+      default: 'Button'
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  data() {
+    return {
+      test: 'test'
+    }
+  },
+  methods: {
+    clickHandler(event) {
+      this.$emit('privet', 'Hello World!!!');
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .base-button {
+    &--primary {
+      background-color: blue;
+    }
+
+    &--secondary {
+      background-color: grey;
+    }
+  }
+</style>
