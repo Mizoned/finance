4cc0a949ffac4081855931b1d199faadcf7f22e4
Valery Shcherbinin <fundroin@mail.ru>
2023-11-13 17:42:54 +0500

    4 добавляет пагинацию для списка валют

--- src/store/CurrenciesStore.js
+++ src/store/CurrenciesStore.js
@@ -4,22 +4,64 @@ import CoinLayer from "@/api/CoinLayer.js";
 export const useCurrenciesStore = defineStore('currenciesStore', {
     state: () => {
         return {
-            currencies: {},
-            isLoading: false
+            currencies: [],
+            page: 1,
+            limit: 10,
+            totalPages: 0,
+            isLoading: false,
         }
     },
     getters: {
-
+        offset() {
+            return this.page * this.limit;
+        },
+        paginatedCurrencies() {
+            return this.currencies?.slice(this.offset - this.limit, this.offset)
+        }
     },
     actions: {
         async getCurrencies() {
-            this.isLoading = true;
-            let response = await CoinLayer.getCurrencies();
-            this.setCurrencies(response?.data?.rates ?? {});
-            this.isLoading = false;
+            let localCurrencies = JSON.parse(localStorage.getItem('currencies'));
+            let currencies = [];
+
+            if (localCurrencies) {
+                currencies = localCurrencies;
+            } else {
+                this.isLoading = true;
+
+                await CoinLayer.getCurrencies()
+                    .then((response) => {
+                        currencies = this.prepareCurrencies(response?.data?.rates ?? {});
+                        localStorage.setItem('currencies', JSON.stringify(currencies));
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+                    .finally(() => {
+                        this.isLoading = false;
+                    });
+            }
+
+            this.totalPages = Math.ceil(currencies.length / this.limit);
+            this.setCurrencies(currencies);
+        },
+        prepareCurrencies(currencies) {
+            let tmp = [];
+
+            Object.keys(currencies).forEach(key => {
+                tmp.push({
+                    name: key,
+                    value: currencies[key]
+                })
+            });
+
+            return tmp;
         },
         setCurrencies(array) {
             this.currencies = array;
+        },
+        setPage(page) {
+            this.page = page;
         }
     }
 });
